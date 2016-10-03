3e0d4b5f3cb871346bcb7f5ff35bda23bd0bdc74
tgrander <treygranderson@gmail.com>
2016-10-03 12:46:21 -0400

    Architect website component layout and render onto to screen as test

---
+++ App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './css/App.css'
+
+import Header from './components/Header'
+import Main from './components/Main'
+import SocialMedia from './components/SocialMedia'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <Header />
+        <Main />
+        <SocialMedia />
+        
+      </div>
+    );
+  }
+}
+
+export default App;
